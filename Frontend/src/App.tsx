4dae69b14692fdc715357f98f3de3722a707e6bf
ravimultani38 <ravi.multani38@gmail.com>
2025-08-20 17:48:28 -0400

    new code

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import SidebarDemo from './components/sidebardemo'
+
+function App() {
+  return (
+    <div className="min-h-screen flex">
+      <SidebarDemo />
+   
+    </div>
+  )
+}
+
+export default App
